89daddfcab5a76d73d09760f6f38298c4b1fc99d
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-04-25 19:13:40 +0530

    fix: update for latest frappe-ui (105)

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
-import { FrappeUI, Button } from 'frappe-ui'
-import router from './router'
-import App from './App.vue'
 import './index.css'
 
+import { createApp } from 'vue'
+import router from './router'
+import App from './App.vue'
+
+import { Button, setConfig, frappeRequest, resourcesPlugin } from 'frappe-ui'
+
 let app = createApp(App)
+
+setConfig('resourceFetcher', frappeRequest)
+
 app.use(router)
-app.use(FrappeUI)
+app.use(resourcesPlugin)
+
 app.component('Button', Button)
 app.mount('#app')
